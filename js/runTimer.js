34efec2b1fab26641fad9bf61c934c793cf34368
HandsomeNut <martinh@mailbox.org>
2019-10-04 22:56:42 +0200

    runtimer function added, timer counting up

---
+++ js/runTimer.js
@@ -0,0 +1,60 @@
+
+var running = false;
+var startDate = 0
+var i = 0;
+
+
+var runTime;
+
+
+timerCount = document.getElementById("timer")
+
+startBtn = document.getElementById("startBtn");
+
+//  timer counting up
+function timer() {
+
+  var now = new Date().getTime();
+
+  var distance = now - startDate;
+
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  if(minutes < 10){
+    minutes = "0" + minutes;
+  }
+
+  if(seconds < 10){
+    seconds = "0" + seconds;
+  }
+
+  timerCount.innerHTML = minutes + ":" + seconds;
+
+  console.log(minutes + ":" + seconds);
+}
+
+// Starts the timer
+function startRun() {
+  runTime = setInterval(timer, 1000);
+};
+
+// stops the timer
+const stopRun = () => {
+  clearInterval(runTime);
+}
+
+
+startBtn.addEventListener("click", evt =>{
+  if(running){
+    startBtn.innerHTML = "play_arrow";
+    running = false;
+    stopRun();
+  } else {
+    startBtn.innerHTML = "pause";
+    running = true;
+    startDate = new Date().getTime();
+    startLoc = navigator
+    startRun();
+  }
+});
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const staticCacheName = "site-static-v2"
+const staticCacheName = "site-static-v3"
 const dynamicCacheName = "site-dynamic-v1"
 const assets = [
   "/",
@@ -15,6 +15,19 @@ const assets = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ]
+
+// cache size limiter
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+        console.log("Ich lösche...")
+      }
+    })
+  })
+};
+
 
 // install event
 self.addEventListener("install", evt =>{
@@ -33,7 +46,7 @@ self.addEventListener("activate", evt =>{
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
       )
     })
