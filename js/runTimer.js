3690f4943069fc620e7e48ab62b0a11375614639
HandsomeNut <martinh@mailbox.org>
2019-10-05 00:40:02 +0200

    hours fixed

--- js/runTimer.js
+++ js/runTimer.js
@@ -18,7 +18,7 @@ function timer() {
 
   var distance = now - startDate;
 
-  var hours = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
